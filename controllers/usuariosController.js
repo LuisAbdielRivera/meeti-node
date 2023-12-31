8960b70d0bda4d602fcc46f3ae1f32ec945c1b65
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2023-12-31 02:41:51 -0600

    update

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const session = require('express-session');
+const flash = require('connect-flash');
+const { body, validationResult } = require('express-validator');
 const Usuarios = require('../models/Usuarios')
 
 exports.formCrearCuenta = (req, res) => {
@@ -8,6 +12,14 @@ exports.formCrearCuenta = (req, res) => {
 
 exports.crearNuevaCuenta = async (req, res) => {
     const usuario = req.body;
+
+    body('confirmar').notEmpty().withMessage('El password confirmado no puede ir vacío');
+    body('confirmar').equals(req.body.password).withMessage('El password es diferente');
+
+    // Obtener los resultados de la validación
+    const erroresExpress = validationResult(req);
+    console.log(erroresExpress)
+
     try{
         const nuevoUsuario = await  Usuarios.create(usuario);
 
@@ -15,6 +27,17 @@ exports.crearNuevaCuenta = async (req, res) => {
     
         console.log('Usuario creado', nuevoUsuario);
     }catch(error){
-        console.log(error);
+
+        // Extraer unicamente el message de los errores
+        const erroresSequelize = error.errors.map(err => err.message)
+
+        // Extraer unicamente el msg de los errores
+        const errExp = erroresExpress.array().map(err => err.msg);
+
+        // Unirlos
+        const listaErrores = [... erroresSequelize, ...errExp]
+
+        req.flash('error', listaErrores);
+        res.redirect('/crear-cuenta');
     }
 }
