e492406446f2dedc1f82218d19900f1a41100cc7
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2023-12-31 00:20:41 -0600

    update

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,5 +1,20 @@
+const Usuarios = require('../models/Usuarios')
+
 exports.formCrearCuenta = (req, res) => {
     res.render('crear-cuenta', {
         nombrePagina: 'Crea tu cuenta'
     })
 };
+
+exports.crearNuevaCuenta = async (req, res) => {
+    const usuario = req.body;
+    try{
+        const nuevoUsuario = await  Usuarios.create(usuario);
+
+        // TODO: Flash Message y redireccionar
+    
+        console.log('Usuario creado', nuevoUsuario);
+    }catch(error){
+        console.log(error);
+    }
+}
--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const router = require('./routes');
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+// Configuración y Modelos BD
 const db = require('./config/db');
-require('./models/Usuarios')
+require('./models/Usuarios');
+db.sync().then(() => console.log('Base de Datos conectada con exito')).catch((error) => console.log(error));
 
-db.sync().then(() => console.log('Base de Datos conectada con exito')).catch((error) => console.log(error))
+// Variables de Desarrollo
+require('dotenv').config({path: 'variables.env'});
 
-require('dotenv').config({path: 'variables.env'})
-
+// Aplicación Principal
 const app = express();
 
+// Body parser, leer formularios
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Habilitar EJS como tempkate engine
-app.use(expressLayouts)
-app.set('view engine', 'ejs')
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // Ubicación vistas
 app.set('views', path.join(__dirname, './views'));
 
 // archivos estaticos
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+// Habilitar cookie parser
+app.use(cookieParser());
+
+// Crear la session
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Agrega flash messages
+app.use(flash());
 
 // Middleware (usuario logueado, flash message, fecha actual)
 app.use((req, res, next) => {
+    res.locals.mensajes = req.flash();
     const fecha = new Date();
     res.locals.year = fecha.getFullYear();
 
@@ -30,9 +56,9 @@ app.use((req, res, next) => {
 })  
 
 // Routing
-app.use('/', router())
+app.use('/', router());
 
 // Agrega el puerto
 app.listen(process.env.PORT, () => {
-    console.log('El servidor esta funcionando')
+    console.log('El servidor esta funcionando');
 })
